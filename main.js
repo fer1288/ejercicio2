84b279440be7772385cfe9477968cc7429ea04b0
fer1288 <fer_12_88@hotmail.com>
2022-09-09 17:39:52 -0300

    no llegue

---
+++ main.js
@@ -0,0 +1,36 @@
+
+const pizza =  [ 
+    {
+    id: 1,
+    nombre: 'Napolitana',
+    ingredientes:[ 'tomate','ajo','aceitunas', 'muzzarella'],
+    precio: 2200
+},
+ { id: 2,
+  nombre: 'Jamon y morrones',
+  ingredientes: ['tomate', 'morrones','jamon','muzzarella'],
+  precio:2100,
+},
+{ id:'3',
+nombre: 'Fugazza',
+ingredientes: ['cebolla','muzzarella'],
+precio:'1800',
+},
+{ id: 4,
+nombre: 'Muzzarella',
+ingredientes:[ 'tomate','muzzarella','oregano',],
+precio: 1700,
+},
+{ id: 5,
+nombre: 'Especial',
+ingredientes: ['tomate',],
+precio:2300,
+},
+{ id: 6,
+    nombre: 'Caribe',
+    ingredientes: ['tomate','muzzarella','Anana'],
+    precio: 2560,
+    },
+
+]
+
